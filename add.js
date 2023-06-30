8e31c35f0343cb1e344a77a7269ce3b4817d312f
ISMAIL QASIM <86529958+Ismacilqasim@users.noreply.github.com>
2023-07-01 00:15:41 +0300

    Update add.js

--- add.js
+++ add.js
@@ -1,15 +1,18 @@
 const option1 = document.getElementById("option1"),
       option2 = document.getElementById("option2"),
       option3 = document.getElementById("option3"),
+      gameMusic = document.getElementById("game-music")
       correct = document.getElementById("correct"),
       wrong = document.getElementById("wrong");
 var answer = 0;
 
+gameMusic.play();
+
 function generate_equation(){ 
   var num1 = Math.floor(Math.random() * 11),
       num2 = Math.floor(Math.random() * 11),
-      dummyAnswer1 = Math.floor(Math.random() * 10),
-      dummyAnswer2 = Math.floor(Math.random() * 10),
+      dummyAnswer1 = Math.floor(Math.random() * 11),
+      dummyAnswer2 = Math.floor(Math.random() * 11),
       allAnswers = [],
       switchAnswers = [];
 
